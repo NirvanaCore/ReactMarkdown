056eb7609e3963facd43a135e5465400af4996ac
soniya rana <soniyasonu0703@gmailo.com>
2021-08-07 22:45:53 +0530

    Markdown code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import marked from 'marked'
 import './App.css';
+import { initialState } from './initialState'
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		text: initialState
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			text: e.target.value
+		});
+	};
+  
+	render() {
+      const { text } = this.state;
+    const markdown = marked(text, { breaks: true });
+    return (
+      <div>
+        <h2 className="text-center m-4 text-white"> Convert your Markdown </h2>
+        <div className="row">
+          <div className="col-6">
+            <h6 className="text-center text-white"> Enter your Markdown: </h6>
+            <textarea
+              className="form-control p-2"
+              id="editor"
+              value={text}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="col-6">
+            <h6 className="text-center text-white"> See the Result:</h6>
+            <div
+              id="preview"
+              className="preview rounded p-2"
+              dangerouslySetInnerHTML={{ __html: markdown }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+	}
 }
 
 export default App;
